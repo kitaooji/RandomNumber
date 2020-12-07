1e2b4a7e5a0c53460cce8763dcff72188551a43c
kitaooji <75258853+kitaooji@users.noreply.github.com>
2020-12-07 20:43:46 +0900

    1st
    add files

--- app/src/main/java/com/example/randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/randomnumber/MainActivity.kt
@@ -19,16 +19,16 @@ class MainActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById<TextView>(R.id.resultTextView)//画面のテキストビュー
         val button: Button = findViewById<Button>(R.id.button)//画面のボタン
 
-
         //0-999のリスト作成,抽選
         val numberList = (1..999).toList()
 
-        //普通のボタン
+        //普通のボタン-タップ
         button.setOnClickListener {
             var result = numberList.random().toString()
             resultTextView.text = result
         }
 
+        //普通のボタン-長押し
         button.setOnLongClickListener {
             if (i === 0) {
                 resultTextView.text = "1000"
@@ -43,17 +43,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun resetNum(view: View) {
-        val resetBtn: Button = findViewById<Button>(R.id.reset)//リセットボタン
-
-        resetBtn.setOnClickListener {
-            i = 0;
-        }
+        val resetBtn: Button = findViewById<Button>(R.id.reset) //リセットボタン
+            resetBtn.setOnClickListener {
+                i = 0;
+            }
     }
 }
-
-
-}
-
 
 
 
